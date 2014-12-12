f9fe5e8a1f8c1b764be96d3f59c9c38a4e9b9685
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-12 17:13:10 +0100

    test(copy-github-labels): add initial unit tests

---
+++ test/copy-github-labels.js
@@ -0,0 +1,11 @@
+var CopyGitHubLabels = require('../');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('CopyGitHubLabels', function(){
+
+	it('should be a function', function(){
+		expect(CopyGitHubLabels).to.be.a('function');
+	});
+
+})
