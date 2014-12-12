3077d34e21d49361863cf6fd04b3151e59146132
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-12 18:07:12 +0100

    test(copy-github-labels): add additional unit tests

--- test/copy-github-labels.js
+++ test/copy-github-labels.js
@@ -8,4 +8,29 @@ describe('CopyGitHubLabels', function(){
 		expect(CopyGitHubLabels).to.be.a('function');
 	});
 
-})
+});
+
+describe('copyGitHubLabels', function(){
+
+	var copyGitHubLabels = new CopyGitHubLabels();
+
+	it('should be an object', function(){
+		expect(copyGitHubLabels).to.be.a('object');
+	});
+
+	describe('#authenticate', function(){
+
+		it('should be a function', function(){
+			expect(copyGitHubLabels.authenticate).to.be.a('function');
+		});
+
+	});
+
+	describe('#copy', function(){
+
+		it('should be a function', function(){
+			expect(copyGitHubLabels.copy).to.be.a('function');
+		});
+
+	});
+});
