f7444195f7e0f8f9faf0a66e91060d74d7bb09cc
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-05-02 11:02:52 +0200

    feat(examples): add counter in output

--- examples/simple.js
+++ examples/simple.js
@@ -8,18 +8,21 @@ var options = {
 // Instantiate with custom options
 var copyGitHubLabels = require('../index.js')(options);
 
-
 var source = 'jvandemo/copy-github-labels';
 var destination = 'your-username/your-repo';
+
+var i=0;
 
 // Copy labels from one repository to another
 copyGitHubLabels.copy(source, destination, function (err, label){
 
+  i++;
+
   // Handle errors
   if(err){
-    return console.log('Could not copy label: ' + JSON.stringify(err));
+    return console.log(i + ': Could not copy label: ' + JSON.stringify(err));
   }
 
   // Copy succeeded
-  console.log('Label copied successfully: ' + JSON.stringify(label))
+  console.log(i + ': Label copied successfully: ' + JSON.stringify(label))
 });
